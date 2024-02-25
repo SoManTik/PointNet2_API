6cf0c0207e93b89ad9edd0e82f9aac6992089a07
So Man Tik <1463647948qq@gmail.com>
2024-02-25 18:40:26 +0800

    fix path bug

--- main.py
+++ main.py
@@ -42,14 +42,14 @@ def load_point_cloud_data(file_path):
 
 
 
-UPLOAD_FOLDER = './PointNet2_API/uploads/'
+UPLOAD_FOLDER = './uploads/'
 ALLOWED_EXTENSIONS = {'txt'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 
-sys.path.append(os.path.join('./PointNet2_API/Pointnet_Pointnet2_pytorch/models'))
+sys.path.append(os.path.join('./Pointnet_Pointnet2_pytorch/models'))
 
 
 
@@ -59,7 +59,7 @@ m = importlib.import_module(model_name)
 model = m.get_model(40, normal_channel=False)
 
 
-checkpoint = torch.load('./PointNet2_API/log/classification/pointnet2_cls_ssg/checkpoints/best_model.pth', map_location='cpu')
+checkpoint = torch.load('./log/classification/pointnet2_cls_ssg/checkpoints/best_model.pth', map_location='cpu')
 start_epoch = checkpoint['epoch']
 model.load_state_dict(checkpoint['model_state_dict'])
 classifier = model.eval()
@@ -84,7 +84,7 @@ def upload_N_detect():
         file = request.files['file']
         # Get the list of files from webpage 
         files = request.files.getlist("file") 
-  
+        print(files)
         try:
             # Iterate for each file in the files List, and Save them 
             for file in files: 
@@ -92,14 +92,14 @@ def upload_N_detect():
         except Exception:
             return "<h1>No files uploaded</h1>"
 
-        file_arr = os.listdir('./PointNet2_API/uploads')
+        file_arr = os.listdir('./uploads')
         file_obj_name = [string for string in file_arr if ".obj" in string]
 
 
         scene = pywavefront.Wavefront(UPLOAD_FOLDER+file_obj_name[0])
         vertices = scene.vertices
         df_obj= pd.DataFrame(vertices)
-        file_path="./PointNet2_API/uploads/txt/uploaded.txt"
+        file_path="./uploads/txt/uploaded.txt"
         df_obj.to_csv(file_path, sep=',', index=False,header=False)
 
 
@@ -162,12 +162,12 @@ def upload_N_detect():
         print(sorted_probabilities_Each_class)
         predict_class = modelnet40_shape_names[sorted_probabilities_Each_class.head(1).index.values[0]]
  
-        # rm all files from ./PointNet2_API/uploads
-        for filename in os.listdir('./PointNet2_API/uploads/'):
-  
-            if os.path.isfile(os.path.join('./PointNet2_API/uploads', filename)):
+        # rm all files from ./uploads
+        for filename in os.listdir('./uploads/'):
+            print(filename)
+            if os.path.isfile(os.path.join('./uploads', filename)):
                 
-                os.remove(os.path.join('./PointNet2_API/uploads', filename))
+                os.remove(os.path.join('./uploads', filename))
 
 
         return render_template('result.html',graphJSON=graphJSON,prediction = predict_class)
