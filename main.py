c9ad198d3d0e868cad2b4d0441a5a510c5a7f41c
So Man Tik <1463647948qq@gmail.com>
2024-03-16 17:13:55 +0800

    fixedbug

--- main.py
+++ main.py
@@ -262,5 +262,5 @@ if __name__ == '__main__':
  
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(debug=False,host="0.0.0.0",port=3000)
+    app.run(debug=True,host="0.0.0.0",port=3000)
 
